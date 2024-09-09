5d375ac523be8284003ee2863008868edafce30b
0xEquinox <williamjmcdonald@protonmail.com>
2024-09-08 22:59:44 -0700

    created a basic tcp server

--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	listenAddr string
+	ln         net.Listener
+	quitch     chan struct{}
+}
+
+func newServer(listenAddr string) *Server {
+	return &Server{
+		listenAddr: listenAddr,
+		quitch:     make(chan struct{}),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+
+	if err != nil {
+		return err
+	}
+
+	defer ln.Close()
+	s.ln = ln
+
+	go s.acceptLoop()
+
+	<-s.quitch
+
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+
+		if err != nil {
+			fmt.Println("An Error Occured While Making a New Connection: ", err)
+			continue
+		}
+
+		fmt.Println("New connection established: ", conn.RemoteAddr())
+
+		go s.readLoop(conn)
+	}
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buf)
+
+		if err != nil {
+			fmt.Println("Error reading connection: ", err)
+			continue
+		}
+
+		msg := buf[:n]
+
+		fmt.Println(string(msg))
+	}
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	server := newServer(":3000")
+	server.Start()
 }
